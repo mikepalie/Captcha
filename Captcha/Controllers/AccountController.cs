6179ba46204aa64d3453125d11e3f2342f391ca1
mikepalie <michalis.palierakis@gmail.com>
2023-11-03 14:12:03 +0200

    Add project files.

---
+++ Captcha/Controllers/AccountController.cs
@@ -0,0 +1,45 @@
+﻿using Captcha.Models;
+using CaptchaMvc.HtmlHelpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Captcha.Controllers
+{
+    public class AccountController : Controller
+    {
+        
+        public ActionResult Index()
+        {
+            var account = new Account();
+            return View("Index", account);
+        }
+
+        [HttpPost]
+        public ActionResult Save(Account account)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View("Index", account);
+            }
+            else
+            {
+                if (!this.IsCaptchaValid(""))
+                {
+                    ViewBag.error = "Invalid Captcha";
+                    return View("Index", account);
+                }
+                else
+                {
+                    ViewBag.account = account;
+                    return View("Success");
+                }
+            }
+        }
+
+
+
+    }
+}
---
+++ Captcha/Models/Account.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Captcha.Models
+{
+    public class Account
+    {
+        [Required]
+        public string Username { get; set; }
+        [Required]
+        public string Password { get; set; }
+        [Required]
+        public string FullName { get; set; }
+    }
+}
